2927c304d31410a9e7978261a8677a81e4d6b2c3
Benji Silver <git@benjifox.gay>
2025-04-13 19:35:34 +0200

    Update to use the Teldrive fork of rclone for Teldrive support

--- scripts/version/Unix-like.sh
+++ scripts/version/Unix-like.sh
@@ -2,7 +2,7 @@
 set -eu
 if [ "$version" = latest ]
 then
-  latest_version=$(gh api repos/rclone/rclone/releases/latest -q .tag_name)
+  latest_version=$(gh api repos/tgdrive/rclone/releases/latest -q .tag_name)
   echo "version=$latest_version" >> $GITHUB_OUTPUT
 else
   echo "version=$version" >> $GITHUB_OUTPUT
--- scripts/version/Windows.ps1
+++ scripts/version/Windows.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 if ($env:version -eq 'latest') {
-  $latest_version = (gh api repos/rclone/rclone/releases/latest -q .tag_name)
+  $latest_version = (gh api repos/tgdrive/rclone/releases/latest -q .tag_name)
   Add-Content $env:GITHUB_OUTPUT version=$latest_version
 }
 else {
